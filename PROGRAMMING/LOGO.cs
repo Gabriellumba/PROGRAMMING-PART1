ecc0d7731b3ecd415d50164c5c1eb808a93fba73
Student <Student@LabVM2047644>
2026-08-26 16:31:25 +0200

    Add project files.

---
+++ PROGRAMMING/LOGO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGRAMMING
+{
+    internal class LOGO
+    {
+        public void displayLogo()
+        {
+            Console.WriteLine("   _____ _   _ _____   _____ _     ");
+         Console.WriteLine("\r\n |_   _| | | | ____| | ____| |    ");
+         Console.WriteLine("\r\n   | | | |_| |  _|   |  _| | |    ");
+        Console.WriteLine(" \r\n   | | |  _  | |___  | |___| |___ ");
+       Console.WriteLine("  \r\n   |_| |_| |_|_____| |_____|_____|");
+
+
+        }
+    }
+}
---
+++ PROGRAMMING/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PROGRAMMING
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        //  wav  extention for audio file 
+
+        {
+            LOGO logo = new LOGO();
+            logo.displayLogo();
+
+            SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\Student\source\repos\PROGRAMMING\PROGRAMMING\voice\Recording.wav");
+
+            player.PlaySync();
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("welcome111");
+
+            string userInput = Console.ReadLine();
+
+        }
+    }
+}
